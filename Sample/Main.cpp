5a5f1762f2379682c2a9a38be626e969a65ac19a
Herlambang Rizky <tandasanyu@gmail.com>
2023-09-02 12:40:44 +0700

    Update
    update latest code

--- Sample/Main.cpp
+++ Sample/Main.cpp
@@ -1,14 +1,44 @@
 #include <iostream>
 #include "OtherFile.cpp"
-using namespace std;
-int main(){
-    outside_function();
-    int number = -4, num2;
+//#using namespace std;
+
+namespace a
+{
+    void printX(){
+        outside_function();
+        int number = -4, num2;
         cout << "number before passing into the function = " << number <<"\n" << endl;
-        number = absolute(number);
+                number = absolute(number);
         cout << "Enter your Number:";
         cin >> num2;
-        number = num2+number;
+                number = num2+number;
         cout << "Final Number : " << number << endl ;
+    }
+}
+
+namespace b
+{
+    void displayN(){
+        //char name1; only work for 1 char
+        string name1; //only work for no space value
+        cout << "Type your full name: ";
+        getline (cin, name1);
+        //cin >> name1;
+        cout << "Your Name : " << name1 << endl ;
+    }
+}
+
+namespace c {
+    void nameD(){
+    char name1 = 'H';
+
+    std::cout << "Your Name : " << name1 << std::endl; // using this code then #using namespace std; should be comment
+    }
+}
+
+int main(){
+ //   a::printX();
+ //   b::displayN();
+c::nameD();
     return 0;
 }
